970e9f0a60afde1ae3adc6054727326adb1a20b1
lei.liu <lei.liu>
2022-06-14 17:57:21 +0800

    use local-gitea+drone

--- build-project-in-drone.sh
+++ build-project-in-drone.sh
@@ -4,7 +4,7 @@ echo "purpose: to see a lot of build in drone-ci"
 
 while true;
 do 
-    curl  -H "Authorization: Bearer 55f24eb3d61ef6ac5e83d550178638dc" -XPOST http://drone.valuetodays.cn/api/repos/valuetodays/projects-for-git/builds
+    curl -XPOST http://192.168.2.203:3080/api/repos/billy/project-for-cicd/builds  -H "Authorization: Bearer 55f24eb3d61ef6ac5e83d550178638dc"
     echo 'sleep 10 seconds....'
     sleep 10
 done
